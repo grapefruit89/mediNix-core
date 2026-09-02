42079dd5d0134fe47f980b114cd3e382b40faa8a
grapefruit89 <49215401+grapefruit89@users.noreply.github.com>
2026-09-02 13:53:04 +0200

    554: logorepo #feishin

--- 55-playback/554-feishin.nix
+++ 55-playback/554-feishin.nix
@@ -9,8 +9,7 @@
 # requires: ["511-caddy"]
 # adr: ADR-554
 # ---
-# No daemon. 511 serves the files because customConfig is set and the
-# registry port is null. Do not write services.caddy.* here.
+# No daemon. Logo: logos/feishin.svg → 518 #feishin.
 { config, lib, pkgs, ... }:
 
 let
@@ -26,17 +25,12 @@ lib.mkIf cfg.enable {
   medinix.ingress.vhosts."feishin" = {
     accessGroup = "stream";
     landing = true;
+    iconId = "feishin";
     customConfig = ''
       root * ${pkgs.feishin-web}/share/feishin-web
       encode off
       try_files {path} /index.html
       file_server
-    '';
-    iconSvg = ''
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-        <rect width="512" height="512" rx="96" fill="#111827"/>
-        <path fill="#38bdf8" d="M160 120h48v176c0 48 40 88 88 88s88-40 88-88-40-88-88-88c-10 0-20 2-28 5V120H160zm136 228c-26 0-48-22-48-48s22-48 48-48 48 22 48 48-22 48-48 48z"/>
-      </svg>
     '';
   };
 }
